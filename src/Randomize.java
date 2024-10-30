b8aa7e7b766ddb8aa178da800906a6f37312fe61
Maria J Kirkou <mariajamalaki@hotmail.com>
2024-10-30 10:08:37 +0100

    maria lagt till en for loop

--- src/Randomize.java
+++ src/Randomize.java
@@ -5,6 +5,10 @@ public class Randomize {
 
     //en metod för att randomizera knapparna 1-15
     public static void randomizeBoard (ArrayList<JButton> buttons) {
+        ArrayList<String> numbers = new ArrayList<>(); // skapar en lista för nr 1-15
+        for (int i = 1; i <= 15; i++) {  //for loop för att gå fr 1-15
+            numbers.add(String.valueOf(i)); // värdet av i omvandlar d till en sträng
+        }
 
     }
 }
