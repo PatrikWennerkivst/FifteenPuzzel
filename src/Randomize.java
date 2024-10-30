d998be390a847444da7cb63ee7e8f6a31f8f5063
Maria J Kirkou <mariajamalaki@hotmail.com>
2024-10-30 10:35:35 +0100

    maria lagt till en shuffle inom collections för att slumpa numrerna

--- src/Randomize.java
+++ src/Randomize.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class Randomize {
 
@@ -9,6 +11,9 @@ public class Randomize {
         for (int i = 1; i <= 15; i++) {  //for loop för att gå fr 1-15
             numbers.add(String.valueOf(i)); // värdet av i omvandlar d till en sträng
         }
+
+        //använder collections för enklaste sättet att slumpa ordningen av numrerna
+        Collections.shuffle(numbers);
 
     }
 }
