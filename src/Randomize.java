4eee2d6d0ffd496be087eadd62cc0420598429e1
Maria J Kirkou <mariajamalaki@hotmail.com>
2024-10-30 09:55:15 +0100

    maria lagt till en metod för att randomizera knapparna

--- src/Randomize.java
+++ src/Randomize.java
@@ -1,2 +1,10 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
 public class Randomize {
+
+    //en metod för att randomizera knapparna 1-15
+    public static void randomizeBoard (ArrayList<JButton> buttons) {
+
+    }
 }
